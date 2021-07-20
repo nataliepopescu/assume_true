43b3af6a9c406a18979e54a95687c16457b1e283
Natalie Popescu <nataliepopescu@gmail.com>
2021-07-19 20:16:36 -0400

    update rustc to after March 2021 and generate THIR

--- forpaper/src/lib.rs
+++ forpaper/src/lib.rs
@@ -1,43 +1,19 @@
-/** Fixed src and dst sizes **/
-
-#[no_mangle]
-pub fn fixed_size(src: &[u8; 320000], dst: &mut [u8; 320000]) {
-    for i in 0..src.len() {
-        dst[i] = src[i];
-    }
-}
-
-/** Both src and dst sizes unknown **/
+/** Checked indexing **/
 
 #[no_mangle]
 pub fn unknown_size(src: &[u8], dst: &mut [u8]) {
     for i in 0..src.len() {
-        dst[i] = src[i];
+        *dst.get_mut(i).unwrap() = *src.get(i).unwrap();
     }
 }
 
-/** Performance motivated unsafe on unknown sizes **/
+/** Unchecked indexing **/
 
 #[no_mangle]
 pub fn perf_mot(src: &[u8], dst: &mut [u8]) {
     for i in 0..src.len() {
         unsafe {
             *dst.get_unchecked_mut(i) = *src.get_unchecked(i);
-        }
-    }
-}
-
-/** Effectively what our transformation will do **/
-
-#[no_mangle]
-pub fn transformation(src: &[u8], dst: &mut [u8]) {
-    if src.len() == dst.len() {
-        for i in 0..src.len() {
-            dst[i] = src[i]
-        }
-    } else {
-        for i in 0..src.len() {
-            dst[i] = src[i]
         }
     }
 }
