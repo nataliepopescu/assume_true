38353d77a7dddae3b39b0c81b99a650ab4a90b74
Natalie Popescu <nataliepopescu@gmail.com>
2021-08-02 21:18:21 -0400

    put dummy unwraps in src/ not benches/

--- forpaper/src/lib.rs
+++ forpaper/src/lib.rs
@@ -11,6 +11,14 @@ pub fn unknown_size(src: &[u8], dst: &mut [u8]) {
 
 #[no_mangle]
 pub fn perf_mot(src: &[u8], dst: &mut [u8]) {
+    let iu8: u8 = 0xF;
+    let some_iu8 = Some(&iu8);
+    let _iu8 = some_iu8.unwrap();
+
+    let mut mu8: u8 = 0xF;
+    let some_mu8 = Some(&mut mu8);
+    let _mu8 = some_mu8.unwrap();
+
     for i in 0..src.len() {
         unsafe {
             *dst.get_unchecked_mut(i) = *src.get_unchecked(i);
